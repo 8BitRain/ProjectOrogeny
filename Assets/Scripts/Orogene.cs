463353f7cb305542b6f4af92ea42ee8d6218ff02
Eric <8BitRain@gmail.com>
2020-09-16 22:28:49 -0500

    Initial Orogeny commit. Added pull and push earth mechanic

---
+++ Assets/Scripts/Orogene.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orogene : MonoBehaviour
+{
+    public float thrust = 1.0f;
+
+    public Transform rock;
+    private Rigidbody rockRB;
+
+    private Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rockRB = rock.GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetButton("Pull")){
+            pull_obj();
+        }
+
+        if(Input.GetButton("Push")){
+            push_obj();
+        }
+        
+    }
+
+    void push_obj()
+    {
+       
+        print("Push");
+        Animator anim = this.GetComponent<Animator>();
+        //anim.StopPlayback;
+        anim.Play("push");
+        //Force
+        //rockRB.AddForce(transform.forward * thrust); 
+        //Move Position
+        //rockRB.MovePosition(rockRB.transform.position + transform.forward * thrust * Time.deltaTime);
+        direction = (rock.transform.position - transform.position).normalized;
+        rockRB.AddForce(direction * thrust); 
+    
+    }
+
+    void pull_obj()
+    {
+        print("Pull");
+        Animator anim = this.GetComponent<Animator>();
+        //anim.StopPlayback;
+        anim.Play("push");
+        //rockRB.MovePosition(rockRB.transform.position - transform.forward * thrust * Time.deltaTime);
+        direction = (rock.transform.position - transform.position).normalized;
+        rockRB.AddForce(-direction * thrust); 
+    }
+
+
+}
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public float speed = 6.0f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    public CharacterController controller;
+    public Transform cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float veritical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, veritical).normalized;
+
+
+        if(direction.magnitude >= .1f){
+            //Atan2 returns angle between x axis and the angle between 0
+            //Gives us an angle in radians
+            //Add the rotation of the camera on the y axis on to the camera
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
