be1c1080b1828d0bc8b3e50431158803270c422d
Eric <8BitRain@gmail.com>
2021-03-10 08:22:45 -0600

    Added basic AI to foes. Experimenting with navMesh and foe logic.

--- Assets/Scripts/Foe.cs
+++ Assets/Scripts/Foe.cs
@@ -1,19 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class Foe : MonoBehaviour
 {
     public Transform Head;
+
+    private NavMeshAgent navMeshAgent;
+    private Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(this.GetComponent<NavMeshAgent>() != null)
+        {
+            this.navMeshAgent = this.GetComponent<NavMeshAgent>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(target == null)
+        {
+            AssignTarget();
+        }
+
+        SeekPlayer();
+    }
+
+    void SeekPlayer()
+    {
+        if(this.navMeshAgent != null && target != null)
+        {
+            transform.LookAt(target);
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+
+    void AssignTarget()
+    {
+        foreach(GameObject obj in GameObject.FindGameObjectsWithTag("P1"))
+        {
+            if(obj.name == "Eston")
+            {
+                target = obj.transform;
+                print("Found Eston, seeking her");
+            }
+        }
     }
 }
