9f5c3d80a6bfdb6921f0e69e83ce194639b1a1ae
Eric <8BitRain@gmail.com>
2021-06-29 21:09:30 -0500

    Added ScreenManager to open and close modals. Added animator controller to control modal state.

--- Assets/Scripts/UI/TutorialController.cs
+++ Assets/Scripts/UI/TutorialController.cs
@@ -9,12 +9,26 @@ public class TutorialController : MonoBehaviour
     public Camera SceneCamera;
     public Canvas canvas;
 
+    public ScreenManager ScreenManager;
+
+    public Image[] modals;
+    private Image currentModal;
+
     public Image startGameModal;
+
+    private bool _tutorialLoopActive = false;
+    private bool _tutorialInitiated = false;
 
 
     /// <summary>Vector2 action for pressing south face button to start game </summary>
     [Tooltip("Vector2 action for South Button ")]
     public InputActionReference StartGamePressed;
+
+    /// <summary>Vector2 action for moving thumbstick </summary>
+    [Tooltip("Vector2 action for South Button ")]
+    public InputActionReference LeftThumbStick;
+
+
     
     // Start is called before the first frame update
     void Start()
@@ -25,10 +39,16 @@ public class TutorialController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(StartGamePressed.action.triggered)
+        if(StartGamePressed.action.triggered && !_tutorialInitiated)
         {
             Debug.Log("Started");
+            //StartGamePressed.action.
             StartGame();
+        }
+
+        if(_tutorialLoopActive)
+        {
+            TutorialLoop();
         }
         
     }
@@ -42,7 +62,55 @@ public class TutorialController : MonoBehaviour
     public void StartTutorial()
     {
         Debug.Log("Tutorial Started");
-        Destroy(startGameModal.gameObject);
+        //Destroy(startGameModal.gameObject);
+
+        if(ScreenManager != null)
+        {
+            ScreenManager.OpenPanel(modals[0].GetComponent<Animator>());
+            _tutorialInitiated = true;
+
+            StartCoroutine(InputWindowCoroutine(1));
+        }
+    
+    }
+
+    public void TutorialLoop()
+    {
+        if(this.ScreenManager.DoesCurrentModalExists())
+        {
+            switch (this.ScreenManager.GetCurrent())
+            {
+                case "Movement Modal":
+                    if(LeftThumbStick.action.ReadValue<Vector2>().x > 0 || LeftThumbStick.action.ReadValue<Vector2>().y > 0)
+                    {
+                        this.ScreenManager.CloseCurrent();
+                        Debug.Log("Close Active Modal");
+                        _tutorialLoopActive = false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void ActivateTutorialLoop()
+    {
+        _tutorialLoopActive = true;
+    }
+
+    IEnumerator InputWindowCoroutine (float time) 
+    {
+        float elapsedTime = 0;
+
+        
+        while (elapsedTime < time)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        ActivateTutorialLoop();
     }
 
 }
